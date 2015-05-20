53aa54d1dda6031b863dd119c06ec7bf4ca3caf4
JannaShaftan <shaftan.j@husky.neu.edu>
2015-05-20 11:00:13 -0700

    template added

---
+++ collider.js
@@ -0,0 +1,27 @@
+//Initialize Game Container
+
+//ENEMIES:
+//number of enemies
+//update enemy location
+//retrieve all current enemy data
+//make circles for each enemy
+//update position
+
+
+//COLLISIONS:
+//Check Score:
+//Check Collision:
+//SetInterval to run checkCollision
+
+
+//ENEMY DATA ARRAY:
+//create the enemy data array
+//loop through number of enemies
+//assign id and a random position
+//returns array of enemy objects
+
+
+//HERO DRAG FUNCTION
+
+
+//ENEMY UPDATER
