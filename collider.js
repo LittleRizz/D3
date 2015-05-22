05769ce2b2c809a91d8abadd1758b5e656e9fbbb
Drew Ruselowski <druselow@gmail.com>
2015-05-22 16:30:00 -0700

    got it all working - need to add boundaries and special effects.  Everything else is functional.

--- collider.js
+++ collider.js
@@ -2,11 +2,12 @@
 var canvasSize = {};
 canvasSize.x = 800;
 canvasSize.y = 500;
+ 
+ 
+var currentScore = 0;
+var highScore = 0;
 
-var speed = 500;
-
-// var dx = d3.event.sourceEvent.pageX;
-// var dy = d3.event.sourceEvent.pageY;
+var speed = 2000;
 
 var gameCanvas= d3.select(".gameCanvas")
 	.attr('width', canvasSize.x)
@@ -16,12 +17,6 @@ var gameCanvas= d3.select(".gameCanvas")
 var Hero = d3.select(".hero")
 		.attr("cx", canvasSize.x / 2)
 		.attr("cy", canvasSize.y / 2);
-//Ohhhh it's drag time, baby.  You ready?  You ready?
-// var dragger = function(){
-// 	d3.select(this)
-// 	 	.attr("cx", d3.event.sourceEvent.pageX)
-// 	 	.attr("cy", d3.event.sourceEvent.pageY);
-// };
 
 // Setting up drag behavior
 var drag = d3.behavior.drag()  
@@ -48,7 +43,7 @@ var enemies = [];
 
 //will push random coordinates into our enemies array
 function createEnemies(){
-	for (var i = 0; i < 1000; i++) {
+	for (var i = 0; i < 20; i++) {
 		enemies.push({
 			"x": getRandomInt(10, 790),
 			"y": getRandomInt(10, 490),
@@ -71,7 +66,7 @@ function enemyUpdate(enemies){
 
 //function to get new values (and sizes) of circles
 function getToDaChoppa() {
-	console.log("get to the choppa!!");
+	// console.log("get to the choppa!!");
 	gameCanvas.selectAll(".enemy")
 	.transition()
 		.duration([speed])
@@ -86,19 +81,61 @@ function getToDaChoppa() {
  	window.setInterval(getToDaChoppa, speed);
  };
 
+var collisionCheck = function(){
+	var enemyList = d3.select('.gameCanvas').selectAll('.enemy');
+	// console.log(enemyList);
+	enemyList.each(function(a, b){
+		// console.log(a, b);
+		var enemyR = parseFloat(d3.select(this).attr('r'));
+		var enemyX = parseFloat(d3.select(this).attr('cx'));
+		var enemyY = parseFloat(d3.select(this).attr('cy'));
+		var heroX = parseFloat(d3.select('.hero').attr('cx'));
+		var heroR = parseFloat(d3.select('.hero').attr('r'));
+		var heroY = parseFloat(d3.select('.hero').attr('cy'));
+		// console.log(heroR, heroX, heroY);
+		var totalDistance = heroR + enemyR;
 
+		var roughDistanceX = Math.sqrt(Math.pow(heroX, 2)) - Math.sqrt(Math.pow(enemyX,2));
+		var finalDistanceX = Math.sqrt(Math.pow(roughDistanceX, 2));
 
+		var roughDistanceY = Math.sqrt(Math.pow(heroY, 2)) - Math.sqrt(Math.pow(enemyY,2));
+		var finalDistanceY = Math.sqrt(Math.pow(roughDistanceY, 2));
+
+		if (finalDistanceX <= totalDistance && finalDistanceY <=totalDistance){
+			d3.select('.current span').text('0');
+			currentScore = 0;
+		};
+		// console.log(totalDistance, finalDistanceX, finalDistanceY);
+			
+		
+	});
+};
+
+var updateScore = function(){
+	var NewCurrentScore = currentScore + 25;
+	console.log(NewCurrentScore);
+	d3.select('.current span').html(NewCurrentScore);
+	d3.select('.high span').html(highScore);
+	currentScore = NewCurrentScore;
+	console.log("current Score:", currentScore)
+	if (currentScore >= highScore){
+		highScore = currentScore;
+		};
+	};
+
+var BestPorkchopSandwichEver = function(){
+	window.setInterval(updateScore, 100);
+};
+
+var checkInterval = function(){
+	window.setInterval(collisionCheck, 100);
+};
 //update enemy location
 //retrieve all current enemy data
 //make circles for each enemy
 //update position
 
-
 //COLLISIONS:
-//Check Score:
-//Check Collision:
-//SetInterval to run checkCollision
-
 
 //ENEMY DATA ARRAY:
 //create the enemy data array
@@ -112,4 +149,11 @@ function getToDaChoppa() {
  //Here is our test shit
 enemyUpdate(createEnemies);
 gametime();
+checkInterval();
+BestPorkchopSandwichEver();
+
+
 //ENEMY UPDATER
+
+//want to set gravity centered on player
+// want objects to collide with each other as well
